8f0c79fa920790c4091f0098d545d432276e4d6f
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2021-06-26 23:06:36 +0530

    Created HackerRank SQL Solutions

---
+++ Advanced Select/New Companies.sql
@@ -0,0 +1,1 @@
+select E.company_code, C.founder, count(distinct lead_manager_code), count(distinct senior_manager_code), count(distinct manager_code), count(distinct employee_code) from Employee E inner join Company C on E.company_code = C.company_code group by E.company_code, C.founder order by E.company_code
